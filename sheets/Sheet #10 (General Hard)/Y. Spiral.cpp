d6abcc544eac32f0b0886e658e82468ff594f184
Khaled Kamr <94804298+khaledkamr@users.noreply.github.com>
2023-11-12 08:39:57 +0200

    corrected the solution of "Y. Spiral.cpp"

--- sheets/Sheet #10 (General Hard)/Y. Spiral.cpp
+++ sheets/Sheet #10 (General Hard)/Y. Spiral.cpp
@@ -1,43 +1,57 @@
 #include <iostream>
 #include <bits/stdc++.h>
+#define ll long long
+#define IOS ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(0);
 using namespace std;
-void fast()
-{
-    cin.tie(0);
-    cout.sync_with_stdio(0);
-}
-bool solve(int x1,int y1,int x2,int y2,int x3,int y3)
-{
-if((x1==x2&&y1==y2)||(x1==x3&&y1==y3)||(x2==x3&&y2==y3))return false;
-    int l1=(((x1-x2)*(x1-x2))+((y1-y2)*(y1-y2)));
-    int l2=(((x1-x3)*(x1-x3))+((y1-y3)*(y1-y3)));
-    int l3=(((x2-x3)*(x2-x3))+((y2-y3)*(y2-y3)));
-    vector<int>v;
-    v.push_back(l1);
-    v.push_back(l2);
-    v.push_back(l3);
-    sort(v.begin(),v.end());
-    int c1=v[0]+v[1];
-    int c2=v[2];
-    return c1==c2;
-}
-int main()
-{
-    fast();
-    int x1,y1,x2,y2,x3,y3;
-    cin>>x1>>y1>>x2>>y2>>x3>>y3;
-    if(solve(x1,y1,x2,y2,x3,y3))cout<<"RIGHT";
-    else if(  solve(x1+1,y1,x2,y2,x3,y3)||solve(x1-1,y1,x2,y2,x3,y3)
-            ||solve(x1,y1+1,x2,y2,x3,y3)||solve(x1,y1-1,x2,y2,x3,y3)
-            ||solve(x1,y1,x2+1,y2,x3,y3)||solve(x1,y1,x2-1,y2,x3,y3)
-            ||solve(x1,y1,x2,y2+1,x3,y3)||solve(x1,y1,x2,y2-1,x3,y3)
-            ||solve(x1,y1,x2,y2,x3+1,y3)||solve(x1,y1,x2,y2,x3-1,y3)
-            ||solve(x1,y1,x2,y2,x3,y3+1)||solve(x1,y1,x2,y2,x3,y3-1)
-           )
-    {
-        cout<<"ALMOST";
-    }
-    else cout<<"NEITHER";
-    return 0;
-}
 
+int main()
+{ 
+	IOS;
+	ll row, col;
+	cin >> row >> col;
+	ll matrix[row][col];
+
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
+			cin >> matrix[i][j];
+		}
+	}
+
+	int r = 0, c = 0;
+
+	while(r < row && c < col)
+	{
+		for(int i = c; i < col; i++)
+		{
+			cout << matrix[r][i] << " ";
+		}
+
+		r++;
+
+		for(int i = r; i < row; i++)
+		{
+			cout<< matrix[i][col -1] << " ";
+		}
+
+		col--;
+
+		if(r < row)
+		{
+			for(int i = col - 1; i >= c; i--)
+			{
+				cout << matrix[row - 1][i] << " ";
+			}
+			row--;
+		}
+		if(c < col)
+		{
+			for(int i = row - 1; i >= r; i--)
+			{
+				cout << matrix[i][c] << " ";
+			}
+			c++;
+		}
+	} 
+}
